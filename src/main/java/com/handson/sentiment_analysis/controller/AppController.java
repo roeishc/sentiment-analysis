9a5defa579ba8f5162ce4a73fd15b2aa117349d3
Roei Shchory <roei.shchory@gmail.com>
2024-07-15 17:26:57 +0300

    added news stream

--- src/main/java/com/handson/sentiment_analysis/controller/AppController.java
+++ src/main/java/com/handson/sentiment_analysis/controller/AppController.java
@@ -1,15 +1,50 @@
 package com.handson.sentiment_analysis.controller;
 
+import com.handson.sentiment_analysis.news.AppNewsStream;
 import com.handson.sentiment_analysis.nlp.SentimentAnalyzer;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.ArrayList;
 
 @RestController
 public class AppController {
 
     @Autowired
     private SentimentAnalyzer sentimentAnalyzer;
+
+    @Autowired
+    private AppNewsStream appNewsStream;
+
+
+    @RequestMapping(path = "/startNewsStream", method = RequestMethod.GET)
+    public @ResponseBody Flux<String> start(String text) throws Exception {
+        return appNewsStream.filter(text)
+                .window(Duration.ofSeconds(3))
+                .flatMap(window -> toArrayList(window))
+                .map(message -> {
+                    if (message.isEmpty())
+                        return "size: 0<br>";
+                    return "size: " + message.size() + "<br>";
+                });
+    }
+
+    @RequestMapping(path = "/stopNewsStream", method = RequestMethod.GET)
+    public @ResponseBody Mono<String> stop(String text) throws Exception {
+        appNewsStream.shutdown();
+        return Mono.just("shutdown");
+    }
+
+    private <T> Mono<ArrayList<T>> toArrayList(Flux<T> source) {
+        return source.reduce(new ArrayList<>(), (a, b) -> {
+            a.add(b);
+            return a;
+        });
+    }
 
     @RequestMapping(path = "/getAnalysis", method = RequestMethod.GET)
     public @ResponseBody Mono<String> getAnalysis(@RequestParam String text)  {
